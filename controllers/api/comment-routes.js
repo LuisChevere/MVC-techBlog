de02897191c5c88a0c288984ba26ad152c2c973a
Luis <hairchevere@gmail.com>
2023-01-25 12:03:43 -0600

    created get route to find all comments

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,3 +1,12 @@
 const router = require('express').Router();
 const { User,Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+//Gets all comments
+router.get('/', (req, res => {
+    Comment.findAll()
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) =>{
+            res.statusCode(500).json(err);
+        });
+}))
