4755aae0f6a3b9ed1461459acf7c04e4abbff89d
Luis <hairchevere@gmail.com>
2023-01-26 12:13:10 -0600

    finished user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -118,3 +118,15 @@ router.post('/login', (req, res) => {
             });
         });
 });
+
+router.post('/logout', (req, res) => {
+    if(req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(404).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+module.exports = router;
