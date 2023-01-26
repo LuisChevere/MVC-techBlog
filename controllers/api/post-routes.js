80e233e08d8ac2b543f1b30b0f063fbf8dd56c93
Luis <hairchevere@gmail.com>
2023-01-26 11:02:58 -0600

    added get route for single post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -21,10 +21,46 @@ router.get('/', (req, res) => {
                 attributes: ['username'],
             },
         },
-    ],
+        ],
     })
-    .then((dbPostData) => res.json(dbPostData))
+        .then((dbPostData) => res.json(dbPostData))
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+});
+
+//Gets a single post
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id,
+        },
+        attributes: ['id', 'content', 'title', 'created_at'],
+        include: [{
+            model: User,
+            attributes: ['username'],
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username'],
+            },
+        },
+        ],
+    })
+    .then((dbPostData) => {
+        if (!dbPostData) {
+            res.status(404).json({
+                message: "No post found with id"
+            });
+            return;
+        }
+        res.json(dbPostData);
+    })
     .catch((err) => {
         res.status(500).json(err);
     });
 });
+
