f4993b454f74a48e18257a898fcab07c0fc8af3f
Luis <hairchevere@gmail.com>
2023-01-26 11:06:53 -0600

    added create post function

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -64,3 +64,17 @@ router.get('/:id', (req, res) => {
     });
 });
 
+//Creates post
+router.post('/', withAuth, (req, res) => {
+    console.log("creating");
+    Post.create({
+        title: req.body.title,
+        content: req.body.post_content,
+        user_id: req.session.user_id
+    })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+        res.status(500).json(err);
+    });
+});
+
