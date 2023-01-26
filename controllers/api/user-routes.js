4f4e35f56fde3bd8d5f10f4e0156db7287d57128
Luis <hairchevere@gmail.com>
2023-01-26 11:20:38 -0600

    created get route for all users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,2 +1,16 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
+
+//Gets all users
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: {
+            exclude: ['password']
+        }
+    })
+    .then(dbUserData = res.json(dbUserData))
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
