f9efa631cef29ca925a3868bba0394f9d950d5f7
Luis <hairchevere@gmail.com>
2023-01-26 11:16:24 -0600

    created delete router and finished post routes js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -99,3 +99,26 @@ router.put('/:id', withAuth, (req, res) => {
         res.json(dbPostData);
     });
 });
+
+//Deletes post
+router.delete('/:id', withAuth, (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id,
+        },
+    })
+    .then((dbPostData) => {
+        if(!dbPostData) {
+            res.status(404).json({
+                message: "No post found with id"
+            });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch((err) => {
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
