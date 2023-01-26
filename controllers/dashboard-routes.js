e02336a73b0a21c7a9e7d9a77631f813f21a25a6
Luis <hairchevere@gmail.com>
2023-01-26 12:36:43 -0600

    created get router for find on post

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -20,13 +20,51 @@ router.get('/', withAuth, (req, res) => {
             model: User,
             attributes: ['username']
         }
-    ]
+        ]
     })
-    .then(dbPostData => {
-        const posts = dbPostData.map(post => post.get({ plain: true}));
-        res.render('dashboard', { posts, loggedIn: true});
-    })
-    .catch(err => {
-        res.status(500).json(err);
-    });
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('dashboard', { posts, loggedIn: true });
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
 });
+
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'title', 'content', 'created_at'],
+        include: [{
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({
+                    message: 'No post found with id'
+                });
+                return;
+            }
+            const post = dbPostData.get({ plain: true });
+
+            res.render('edit-post', { post, loggedIn: true });
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+
+
+})
