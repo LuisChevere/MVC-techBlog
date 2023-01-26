3bfe1aaada1b51f862ec8f6f3b512db42f78466f
Luis <hairchevere@gmail.com>
2023-01-26 11:36:33 -0600

    creating create user router and post

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -50,3 +50,23 @@ router.get('/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+//Creates user
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id =dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+})
