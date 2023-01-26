3c452542be2bf0bef1253f7ca72b432dd2604665
Luis <hairchevere@gmail.com>
2023-01-26 10:56:40 -0600

    added get route for post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,3 +1,30 @@
 const router = require('express').Router();
-const { User,Post, Comment } = require('../../models');
+const { User, Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+//Gets all posts
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id', 'content', 'title', 'created_at'],
+        order: [
+            ['created_at', 'DESC']
+        ],
+        include: [{
+            model: User,
+            attributes: ['username'],
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username'],
+            },
+        },
+    ],
+    })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+        res.status(500).json(err);
+    });
+});
