357335063468089b12d601efedac07caba13ea4c
Luis <hairchevere@gmail.com>
2023-01-26 17:52:12 -0600

    working on get router for post id

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -18,10 +18,46 @@ router.get('/', (req, res) => {
         }
         ]
     })
-    .then(dbPostData => {
-        const posts = dbPostData.map(post => post.get({ plain: true}));
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
 
-        res.render('homepage', {posts, loggedIn: req.session.loggedIn});
+            res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'title', 'content', 'created_at'],
+        include: [{
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }
+        ]
+    })
+    .then(dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({
+                message: 'No post found with id'
+            });
+            return;
+        }
+
+        const post = dbPostData.get({ plain: true});
+        res.render('single-post', {post, loggedIn: req.session.loggedIn});
     })
     .catch(err => {
         res.status(500).json(err);
