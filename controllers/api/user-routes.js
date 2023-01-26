cc815c492a05dcd5a3f0e575376cf97a7d003505
Luis <hairchevere@gmail.com>
2023-01-26 11:31:44 -0600

    created get route for a specific user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -8,9 +8,45 @@ router.get('/', (req, res) => {
             exclude: ['password']
         }
     })
-    .then(dbUserData = res.json(dbUserData))
-    .catch(err => {
-        res.status(500).json(err);
-    });
+        .then(dbUserData = res.json(dbUserData))
+        .catch(err => {
+            res.status(500).json(err);
+        });
 });
 
+//Get specific user
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: {
+            exclude: ['password']
+        },
+        where: {
+            id: req.params.id
+        },
+        include: [{
+            model: Post,
+            attributes: ['id', 'title', 'content', 'created_at']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'created_at'],
+            include: {
+                model: Post,
+                attributes: ['title']
+            }
+        }
+        ]
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({
+                    message: 'No user found with id'
+                });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
