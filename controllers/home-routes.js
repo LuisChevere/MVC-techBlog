dafd8ee2285115fb575af7839f21edd4fbf33536
Luis <hairchevere@gmail.com>
2023-01-29 14:34:52 -0600

    fixed pathing issue to models

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Comment } = require('../../models');
+const { User, Post, Comment } = require('../models');
 
 //gets home route
 router.get('/', (req, res) => {
